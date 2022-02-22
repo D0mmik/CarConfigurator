f625b3dd70ff51b783e19106193544fed1b990bf
D0mmik <dominik.strnadel@gmail.com>
2022-02-22 21:18:50 +0100

    Initial commit

---
+++ Assets/Scripts/WheelChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class WheelChanger : MonoBehaviour
+{
+    public GameObject[] wheels;
+    public WheelButton WheelButton;
+    public GameObject currentclone;
+
+    void Start()
+    {
+        //wheels = GameObject.FindGameObjectsWithTag("wheel");
+
+        for(int i = 0; i < wheels.Length; i++)
+        {   
+            var WheelButtonClone = Instantiate(WheelButton, transform).GetComponent<WheelButton>();
+            WheelButtonClone.ButtonInt = i;  
+            WheelButtonClone.Button.onClick.AddListener(()=>dothis(WheelButtonClone.ButtonInt));        
+        }
+    }
+    public void dothis(int number)
+    {  Destroy(currentclone);
+       Debug.Log(number);
+       currentclone = Instantiate(wheels[number]);
+    }
+
+
+}
