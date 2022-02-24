9f54c5db685011fa7f74985be179a6d3d6bd7ee0
D0mmik <dominik.strnadel@gmail.com>
2022-02-24 20:17:04 +0100

    ColorChanger

---
+++ CarConfiguratorProject/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material[] colors;
+    public ColorButton ColorButton;
+    public GameObject Car;
+
+    void Start()
+    {
+         for(int i = 0; i < colors.Length; i++)
+        {   
+            var ColorButtonClone = Instantiate(ColorButton, transform).GetComponent<ColorButton>();
+            ColorButtonClone.ButtonInt = i;
+            ColorButtonClone.ColorName.text = ($"COLOR {ColorButtonClone.ButtonInt}");  
+            ColorButtonClone.Button.onClick.AddListener(()=>dothis(ColorButtonClone.ButtonInt));                    
+        }
+    }
+    public void dothis(int number)
+    {  
+        Debug.Log(number);
+        Car.GetComponent<MeshRenderer>().material = colors[number];
+
+    }
+}
--- CarConfiguratorProject/Assets/Scripts/UIButtons.cs
+++ CarConfiguratorProject/Assets/Scripts/UIButtons.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIButtons : MonoBehaviour
 {
     public GameObject Wheels;
+    public GameObject Colors;
+    public GameObject ColorsScrollbar;
 
     public void WheelButton()
     {
         Wheels.SetActive(true);
+        Colors.SetActive(false);
+        ColorsScrollbar.SetActive(false);
     }
     public void ColorButton()
     {
-       Wheels.SetActive(false); 
+       Colors.SetActive(true);
+       ColorsScrollbar.SetActive(true);
+       Wheels.SetActive(false);
+
     }
 }
